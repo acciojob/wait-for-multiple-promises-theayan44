f5700ee0678968fafd0fac7dffaf8ed905930aa1
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-24 18:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const initialText = document.createElement("tr");
-initialText.innerHtml = `<td>Loading...</td>`;
+initialText.innerHTML = `<td>Loading...</td>`;
 output.appendChild(initialText);
 const promises = [];
 for(let i=0; i<3; i++){
@@ -14,14 +14,14 @@ Promise.all(promises).then((values)=>{
 	initialText.remove();
 	for(let i=0; i<values.length; i++){
 		const tr = document.createElement("tr");
-		tr.innerHtml = `
+		tr.innerHTML = `
 			 <td>${values[i]}</td>
 			 <td>${i+1}</td>
 		 `;
 		output.appendChild(tr);
 	}
 	const total = document.createElement("tr");
-	total.innerHtml = `<td>Total</td>
+	total.innerHTML = `<td>Total</td>
 					 <td>3.0</td>`;
 	output.appendChild(total);
 })
